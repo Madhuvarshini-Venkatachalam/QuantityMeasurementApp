706dcbc38fb10c935ed55aed25a17ac023e56bbc
narayananvenkatesh05-jr <narayananvenkatesh05@gmail.com>
2026-04-23 19:12:52 +0530

    Initial setup

---
+++ src/main/java/QuantityMeasurementApp.java
@@ -0,0 +1,35 @@
+package com.quantity;
+
+public class QuantityMeasurementApp {
+
+    // Inner class
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            // Same reference
+            if (this == obj) return true;
+
+            // Null or different type
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Feet feet = (Feet) obj;
+
+            return Double.compare(this.value, feet.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Equal: " + f1.equals(f2));
+    }
+}
